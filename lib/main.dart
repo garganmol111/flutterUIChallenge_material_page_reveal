93f13361a3e1012416b1346aa0547c0423a0d6a1
garganmol111 <anmol.169105041@muj.manipal.edu>
2019-04-21 21:37:40 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Material Page Reveal',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MaterialPageReveal(title: 'Flutter Demo Home Page'),
+      home: MaterialPageReveal(title: 'Material Page Reveal Home Page'),
     );
   }
 }
